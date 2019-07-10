86710bbe8586de7a90ef3d10921ff826acbf7e94
KathirvelChandrasekaran <kathirvel98@outlook.com>
2019-07-10 10:03:42 +0530

    Add files via upload

---
+++ LoginAuthentication/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCm4TfxHRsmfzxddDoSqWrkIzYW0fgliQ4",
+    authDomain: "loginauth-587f3.firebaseapp.com",
+    databaseURL: "https://loginauth-587f3.firebaseio.com",
+    projectId: "loginauth-587f3",
+    storageBucket: "",
+    messagingSenderId: "867472994160",
+    appId: "1:867472994160:web:a4d13d45e52737ea"
+  }
+};
